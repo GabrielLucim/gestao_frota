b8d2000111980680597e4c9ce82a82efff689184
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-04-05 00:14:44 -0300

    7 telas

--- lib/modelos/motorista.dart
+++ lib/modelos/motorista.dart
@@ -1,6 +1,13 @@
 class Motorista {
   String nome;
   String categoria_cnh;
+  String cpf;
+  int idade;
 
-  Motorista({required this.nome, required this.categoria_cnh});
+  Motorista({
+    required this.nome,
+    required this.categoria_cnh,
+    required this.cpf,
+    required this.idade,
+  });
 }
