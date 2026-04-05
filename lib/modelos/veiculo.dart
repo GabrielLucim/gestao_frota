5e1d7fc0a508c0abd79eb3c89d9886f1c564d0ab
GabrielLucim <20241PVAI10030005@estudantes.ifpr.edu.br>
2026-04-05 00:03:22 -0300

    salvando

--- lib/modelos/veiculo.dart
+++ lib/modelos/veiculo.dart
@@ -2,14 +2,14 @@ class Veiculo {
   String placa;
   String modelo;
   String fabricante;
-  int anoTESTE;
+  int ano;
   double kmRodados;
 
   Veiculo({
     required this.placa,
     required this.modelo,
     required this.fabricante,
-    required this.anoTESTE,
+    required this.ano,
     required this.kmRodados,
   });
 }
